b453c5c9580ea99f3e7db50889c481a2356c742c
Akash Gupta <akashgupta7363@gmail.com>
2023-04-21 20:14:25 +0530

    created chatModel

---
+++ Back-end/models/chatModels.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+const Chat = mongoose.model("Chat", chatModel);
+module.exports = Chat;
