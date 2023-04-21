e3ec9998be78cef2a4e7e2197b10221da8bb83b2
Akash Gupta <akashgupta7363@gmail.com>
2023-04-21 16:48:48 +0530

    button create using chakra ui

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
+}
+
+export default App;
