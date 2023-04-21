58dfff5ce8d0d05aa38f7db2893cf02d7ec496df
Akash Gupta <akashgupta7363@gmail.com>
2023-04-21 16:36:35 +0530

    Serever created

---
+++ Back-end/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const chats = require("./Dummy data/data");
+
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("APP IS RUNNING");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id == req.params.id);
+  res.send(singleChat);
+});
+
+PORT = process.env.PORT || 7000;
+app.listen(PORT, console.log(`Server is listenning on port ${PORT}`));
