c7faf7e4a12357bb80c517c138a63be4376c45e2
Akash Gupta <akashgupta7363@gmail.com>
2023-04-22 18:37:05 +0530

    Created login guest auto fill credentials

--- front-end/src/component/Authentication/Login.js
+++ front-end/src/component/Authentication/Login.js
@@ -8,13 +8,67 @@ import {
   Button,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useToast } from "@chakra-ui/react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function Login() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const history = useHistory();
   const handleClick = () => setShow(!show);
-  const submitHandler = () => {};
+  const submitHandler = async () => {
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "Please fill all the fields",
+
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+      toast({
+        title: "login Successful",
+
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured  somewhere",
+        description: error.response.data.message,
+
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
 
   return (
     <>
@@ -24,6 +78,7 @@ function Login() {
           <Input
             placeholder="Enter Your Email"
             onChange={(e) => setEmail(e.target.value)}
+            value={email}
           ></Input>
         </FormControl>
         <FormControl id="checkpassword" isRequired>
@@ -33,6 +88,7 @@ function Login() {
               type={show ? "text" : "password"}
               placeholder="Enter Your Email"
               onChange={(e) => setPassword(e.target.value)}
+              value={password}
             ></Input>
             <InputRightElement width={"4rem"}>
               <Button h="1.7rem" size={"sm"} onClick={handleClick}>
@@ -47,6 +103,7 @@ function Login() {
           width={"100%"}
           style={{ marginTop: 15 }}
           onClick={submitHandler}
+          isLoading={loading}
         >
           Login
         </Button>
