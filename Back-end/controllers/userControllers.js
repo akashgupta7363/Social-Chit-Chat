498257617a2da6127b889ecafaee388af709c107
Akash Gupta <akashgupta7363@gmail.com>
2023-04-22 13:49:21 +0530

    login authentication aprt done

--- Back-end/controllers/userControllers.js
+++ Back-end/controllers/userControllers.js
@@ -36,7 +36,7 @@ const registerUser = asynHandler(async (req, res) => {
 const authUser = asynHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
-  if (user && User.matchPassword(password)) {
+  if (user && user.matchPassword(password)) {
     res.status(201).json({
       _id: user._id,
       name: user.name,
