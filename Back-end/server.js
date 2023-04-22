f27b7b00b1b69eb8a515739c4ceb85de3b414713
Akash Gupta <akashgupta7363@gmail.com>
2023-04-22 11:27:05 +0530

    created user routes

--- Back-end/server.js
+++ Back-end/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const chats = require("./Dummy data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 connectDB();
@@ -12,14 +13,16 @@ app.get("/", (req, res) => {
   res.send("APP IS RUNNING");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use("api/user", userRoutes);
 
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id == req.params.id);
-  res.send(singleChat);
-});
+// app.get("/api/chat", (req, res) => {
+//   res.send(chats);
+// });
+
+// app.get("/api/chat/:id", (req, res) => {
+//   const singleChat = chats.find((c) => c._id == req.params.id);
+//   res.send(singleChat);
+// });
 
 PORT = process.env.PORT || 7000;
 app.listen(
