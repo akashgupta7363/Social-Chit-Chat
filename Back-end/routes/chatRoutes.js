68c406ec34675b322323ac07bd173f0a6ac71651
Akash Gupta <akashgupta7363@gmail.com>
2023-04-22 21:41:35 +0530

    defined chat routing

---
+++ Back-end/routes/chatRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const protect = require("../middleware/authMiddleware");
+const router = express.Router();
+
+// router.route('/').post(protect,accessChat).get(protect,fetchChats);
+// router.route('/group').post(protect, createGroupChat);
+// router.route('/rename').put(protect,renameGroup);
+// router.route('/groupremove').put(protect,removeGroup);
+// router.route('/addgroup').put(protect,addToGroup);
