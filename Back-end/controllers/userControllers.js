fc66540e4942b8f49d47184b9fd155d84e2e1a76
Akash Gupta <akashgupta7363@gmail.com>
2023-04-22 11:54:12 +0530

    created controller for user

---
+++ Back-end/controllers/userControllers.js
@@ -0,0 +1,33 @@
+const asynHandler = require("express-async-handler");
+
+const registerUser = asynHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the fields");
+  }
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("User alredy exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create user ");
+  }
+});
+
+module.exports = { registerUser };
--- Back-end/routes/userRoutes.js
+++ Back-end/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const { registerUser } = require("../controllers/userControllers");
 const router = express.Router();
 
-// router.route("/").post(registerUser);
+router.route("/").post(registerUser);
 // router.post("/login", authUser);
 
 module.exports = router;
