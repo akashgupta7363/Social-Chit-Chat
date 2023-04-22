94591df581e0ded7d630f207283410cf84d91c7b
Akash Gupta <akashgupta7363@gmail.com>
2023-04-22 19:50:53 +0530

    route and controllers for seraching

--- Back-end/controllers/userControllers.js
+++ Back-end/controllers/userControllers.js
@@ -49,5 +49,17 @@ const authUser = asynHandler(async (req, res) => {
     throw new Error("Invalid amil or password");
   }
 });
+const allUsers = asynHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
 
-module.exports = { registerUser, authUser };
+module.exports = { registerUser, authUser, allUsers };
--- Back-end/routes/userRoutes.js
+++ Back-end/routes/userRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userControllers");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(allUsers);
 router.post("/login", authUser);
 
 module.exports = router;
