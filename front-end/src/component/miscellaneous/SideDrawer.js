ed4b594dec5b4a3bbbb364a2f2a1580890b4e414
Akash Gupta <akashgupta7363@gmail.com>
2023-04-24 17:00:52 +0530

    added header

--- front-end/src/component/miscellaneous/SideDrawer.js
+++ front-end/src/component/miscellaneous/SideDrawer.js
@@ -1,24 +1,69 @@
 import React, { useState } from "react";
-import { Box, Button, Text, Tooltip } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { ChatState } from "../../Context/ChatProvider";
 
 function SideDrawer() {
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingChats, setLoadingChats] = useState("");
+  const { user } = ChatState();
 
   return (
     <div>
-      <Box>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        bg={"white"}
+        w={"100%"}
+        padding={"5px 10px 5px 10px"}
+        borderWidth={"5px"}
+      >
         <Tooltip label="Search users to chat" hasArrow placement="bottom-end">
           <Button variant={"ghost"}>
             <i class="fas fa-search"></i>
             <Text display={{ base: "none", md: "flex" }} px={"4"}>
-              {" "}
               Search user
             </Text>
           </Button>
         </Tooltip>
+        <Text fontSize={"2xl"} fontFamily={"Work sans"}>
+          Chatty
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon fontSize="2xl " m={1} />
+            </MenuButton>
+            <MenuList></MenuList>
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size={"sm"}
+                cursor={"pointer"}
+                name={user.name}
+                src={user.pic}
+              ></Avatar>
+            </MenuButton>
+            <MenuList>
+              <MenuItem>My Profile</MenuItem>
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
       </Box>
     </div>
   );
