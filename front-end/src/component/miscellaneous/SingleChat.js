2e018d6bf5fdfc9ca8cba006e71396cd0a4d1724
Akash Gupta <akashgupta7363@gmail.com>
2023-04-26 19:39:11 +0530

    defaut selecting chat

--- front-end/src/component/miscellaneous/SingleChat.js
+++ front-end/src/component/miscellaneous/SingleChat.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { ChatState } from "../../Context/ChatProvider";
+import { Box, Text } from "@chakra-ui/react";
 
-function SingleChat({ fetchagain, setFetchagain }) {
+const SingleChat = ({ fetchagain, setFetchagain }) => {
   const { users, selectedChat, setSelectedChat } = ChatState();
 
-  return <div>SingleChat</div>;
-}
-
+  return (
+    <>
+      {selectedChat ? (
+        <></>
+      ) : (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          h="100%"
+        >
+          <Text fontSize="3xl" pb={3} fontFamily="Work sans">
+            Click on a user to start chatting
+          </Text>
+        </Box>
+      )}
+    </>
+  );
+};
 export default SingleChat;
